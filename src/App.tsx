1546d9b3f8be818393eafa4c5e624beee9b9b205
Konrad Fritsche <fritsche@nextbike.com>
2022-01-24 09:35:23 +0100

    zweiter-workshop | prepare app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,14 @@ import { GalleryViewComponent } from './view/gallery-view.component';
 import { CameraViewComponent } from './view/camera-view.component';
 
 export class App {
-  //erstmal grundsätzlich was über das projekt erzählen und die struktur
+
   public start() {
-    //Hier vielleicht was machen? ToggleCameraView einfügen
+    
     contentView.append(
       <$>
         <TextView padding={15} font='bold 24px monospace' centerX>My Photo Gallery</TextView>
         <GalleryViewComponent top={Constraint.prev} stretch />
-        <Button centerX bottom={15} height={50} width={150} cornerRadius={15} style='elevate' onSelect={() => this.toggleCameraView()}>
+        <Button centerX bottom={15} height={50} width={150} cornerRadius={15} style='elevate'>
           Open Camera
         </Button>
       </$>
